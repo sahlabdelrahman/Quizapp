3fc1375873907c80768d72678a6de9ea0cc2e973
sahlabdelrahman <bnabdelrahman07@gmail.com>
2020-05-01 16:51:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+import Home from './components/Home';
+import QuizInstructions from './components/quiz/QuizInstructions';
+import Play from './components/quiz/Play';
+import QuizSummary from './components/quiz/QuizSummary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/play/instructions' component={QuizInstructions} />
+        <Route path='/play/quiz' component={Play} />
+        <Route path='/play/quizSummary' component={QuizSummary} />
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
+import './styles/styles.scss';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
